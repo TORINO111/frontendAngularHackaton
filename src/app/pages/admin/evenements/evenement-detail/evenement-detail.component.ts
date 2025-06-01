af3c4a3d0315452f3556378596b6413e71d30d47
TORINO <victorinattolode@gmail.com>
2025-06-01 02:14:10 +0000

    Redirection vers page voir details avec données evenement

--- src/app/pages/admin/evenements/evenement-detail/evenement-detail.component.ts
+++ src/app/pages/admin/evenements/evenement-detail/evenement-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
+import { EvenementService } from '../../../../shared/services/evenements/impl/evenement.service';
 import { Evenement } from '../../../../shared/models/evenement.model';
 import { SidebarComponent } from "../../../../shared/components/layout/sidebar/sidebar.component";
 
@@ -10,17 +11,37 @@ import { SidebarComponent } from "../../../../shared/components/layout/sidebar/s
   imports: [RouterModule, SidebarComponent]
 })
 export class EvenementDetailComponent implements OnInit {
+  evenementId: string | null = null;
   evenement: Evenement | undefined;
   errorMessage: string = '';
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private evenementService: EvenementService
+  ) { }
 
   ngOnInit(): void {
-    this.route.data.subscribe((data) => {
-      this.evenement = data['evenement'];
+    this.evenementId = this.route.snapshot.paramMap.get('id');
+    if (this.evenementId) {
+      this.loadEvenementDetails(this.evenementId);
       console.log(this.evenement)
-      if (!this.evenement) {
-        this.errorMessage = 'Détails de l\'événement non trouvés.';
+    } else {
+      this.errorMessage = 'ID de l\'événement non trouvé dans l\'URL.';
+    }
+  }
+
+  loadEvenementDetails(id: string): void {
+    this.evenementService.getEvenementById(id).subscribe({
+      next: (response) => {
+        if (response && response.data) {
+          this.evenement = response.data;
+        } else {
+          this.errorMessage = 'Détails de l\'événement non trouvés.';
+        }
+      },
+      error: (error) => {
+        console.error('Erreur lors de la récupération des détails de l\'événement:', error);
+        this.errorMessage = 'Erreur lors du chargement des détails de l\'événement.';
       }
     });
   }
