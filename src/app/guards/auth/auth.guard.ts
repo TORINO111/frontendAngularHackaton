c6317d36b826fb29f6ec0bb8882dbbb773477b56
TORINO <victorinattolode@gmail.com>
2025-06-03 12:59:35 +0000

    Intégration jwt du backend

--- src/app/guards/auth/auth.guard.ts
+++ src/app/guards/auth/auth.guard.ts
@@ -1,10 +1,10 @@
   import { Injectable } from '@angular/core';
   import { CanActivate, Router } from '@angular/router';
-import { AuthenticationService } from '../../shared/services/auth/impl/authentication.service';
+import { AuthService } from '../../shared/services/auth/impl/authentication.service';
 
   @Injectable({ providedIn: 'root' })
   export class AuthGuard implements CanActivate {
-    constructor(private authService: AuthenticationService, private router: Router) {}
+    constructor(private authService: AuthService, private router: Router) {}
 
     canActivate(): boolean {
       if (this.authService.isAuthenticated()) {
